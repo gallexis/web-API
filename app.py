91fbb99828a5361de0cdb815e98d3d75cff58f52
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-02-10 11:18:26 +0100

    Passeur fonctionnel

--- app.py
+++ app.py
@@ -2,20 +2,73 @@ __author__ = 'alexisgallepe'
 
 import os
 from bottle import route, run, template
+import xmlParser
+import requests
+import json
 
-index_html = '''My first web app! By <strong>{{ author }}</strong>.'''
 
+publications = []
 
 @route('/')
 def index():
-    return template(index_html, author='Real Python')
+    return "eee"
 
 
-@route('/name/<name>')
-def name(name):
-    return template(index_html, author=name)
+@route('/search/arbres/<query>')
+def search_trees(query):
+    trees100 = []
+    params = ["hauteur","circonference"]
+    param = query.split(":")
 
+    value = param[1]
+    param = param[0]
+
+    if param not in params:
+        return "error"
+
+    for tree in parsedTrees:
+        if tree[param] == value and len(trees100) < 100:
+            trees100.append(tree)
+
+
+    return json.dumps(trees100)
+
+
+@route('/title/<url:path>')
+def title(url):
+    r = requests.get(url)
+
+    parser = MyHTMLParser()
+    parser.feed(r.text)
+
+    return "Titre: " + parser.title
+
+
+@route('/arbre/<id:int>')
+def get_tree_by_id(id):
+    min = 0
+    max = len(parsedTrees)
+    print(max)
+
+    if id <= max and id >= min:
+        return parsedTrees[id]
+    else:
+        return "error"
+
+
+@route('/licence')
+def content():
+    return json.dumps(parsedTrees)
+
+def parseFile():
+    parser = xmlParser.XmlParser()
+    parser.parse("sample.xml",50)
+    return parser.publications
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    print("Parsing File")
+    publications = parseFile()
+
+
+    port = int(os.environ.get('PORT', 1337))
     run(host='0.0.0.0', port=port, debug=True)
---
+++ xmlParser.py
@@ -0,0 +1,66 @@
+__author__ = 'alexisgallepe'
+import xml.sax
+from datetime import date
+
+class XmlParser(xml.sax.ContentHandler):
+    def __init__(self):
+        super().__init__()
+
+        self.last_years_wanted = 0
+
+        self.publicationBalise = ""
+        self.fieldBalise = ""
+
+        self.publications_index = ["article","inproceedings","proceedings","book","incollection","phdthesis","masterthesis"]
+        self.fields_index = ["author", "title", "year", "journal","booktitle"]
+
+        self.publication = {"author":"", "title":"", "year":"", "journal":"","booktitle":""}
+        self.publications = []
+
+    def parse(self, sourceFileName, last_years_wanted):
+        self.last_years_wanted = last_years_wanted
+        source = open(sourceFileName)
+        xml.sax.parse(source, self)
+
+    def startElement(self, name, attrs):
+        try:
+
+            if name in self.publications_index:
+                self.publicationBalise = name
+
+            if name in self.fields_index:
+                self.fieldBalise = name
+
+        except Exception as e:
+            print(e)
+            pass
+
+    def endElement(self, name):
+        try:
+
+            if self.publicationBalise == name:
+                self.publicationBalise = ""
+
+                # if (2016 - 5) <= 2001  ---> False
+                if not self.publication["year"] == '' and (date.today().year - self.last_years_wanted) <= int(self.publication["year"]):
+                    self.publications.append(self.publication)
+                    self.publication = {"author":"", "title":"", "year":"", "journal":"","booktitle":""}
+
+            if self.fieldBalise == name:
+                self.fieldBalise = ""
+
+        except Exception as e:
+            print(e)
+            pass
+
+
+    def characters(self, content):
+        try:
+            if not self.fieldBalise == "":
+                self.publication[self.fieldBalise] = content
+
+        except Exception as e:
+            print(e)
+            pass
+
+
