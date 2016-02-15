1764298db4377f8bbd45ce591bc8a19da4a18147
Alexis Gallèpe <alexis.gallepe@gmail.com>
2016-02-15 22:46:32 +0100

    regex terminées

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bottle import route, run, template, abort
 import xmlParser
 import requests
 import json
+import re
 
 
 publications = []
@@ -13,7 +14,6 @@ publications = []
 @route('/publications/<id:int>')
 def get_publication(id):
 
-    global publications
     min = 0
     max = len(publications)
     print(max)
@@ -23,59 +23,69 @@ def get_publication(id):
         abort(401,"problem with id parameter")
 
 
-@route('/authors/<name>')
-def get_author_infos(name):
-    global publications
+@route('/authors/<author_name>')
+def get_author_infos(author_name):
     nb_publications = 0
     nb_co_authors = 0
 
     for publication in publications:
-        if name in publication["authors"] :
-            print(publication)
+        if author_name in publication["authors"] :
             nb_publications+=1
             nb_co_authors = len(publication["authors"])
-            print(publication["authors"])
 
     return json.dumps({ "number_publications":nb_publications , "number_co_authors":nb_co_authors })
 
+# ?? return uniquement les titles ??
+@route('/authors/<author_name>/publications')
+def get_author_publications(author_name):
 
-@route('/authors/<name>/publications')
+    list_publications = []
 
-@route('/authors/<name>/coauthors')
+    for publication in publications:
+        if author_name in publication["authors"]:
+            list_publications.append(publication["title"])
 
-@route('/search/authors/{searchString}')
-
-@route('/search/publications/{searchString}')
-
-@route('/authors/{name_origine}/distance/{name_destination}')
-
-
-
+    return json.dumps({ "author":author_name , "publications":list_publications })
 
 
-@route('/')
-def index():
-    return "eee"
+
+@route('/authors/<author_name>/coauthors')
+def get_coauthors(author_name):
+
+    list_coauthors = set()
+
+    for publication in publications:
+        if author_name in publication["authors"]:
+
+            for author in publication["authors"]:
+                list_coauthors.add(author)
+
+    list_coauthors.remove(author_name)
+
+    return json.dumps({ "author":author_name , "coauthors": list(list_coauthors) })
 
 
-@route('/search/arbres/<query>')
-def search_trees(query):
-    trees100 = []
-    params = ["hauteur","circonference"]
-    param = query.split(":")
+@route('/search/authors/<searchString>')
+def search_authors(searchString):
 
-    value = param[1]
-    param = param[0]
+    regex = searchString.replace("%","(.)").replace("*","(.)*")
+    for publication in publications:
+        for author in publication["authors"]:
+            print(regex,' : ',author)
+            if re.match(regex, author):
+                return get_author_infos(author)
 
-    if param not in params:
-        return "error"
-
-    for tree in parsedTrees:
-        if tree[param] == value and len(trees100) < 100:
-            trees100.append(tree)
+    return json.dumps({ "error": "Author not found" })
 
 
-    return json.dumps(trees100)
+@route('/search/publications/<searchString>')
+
+@route('/authors/<name_origine>/distance/<name_destination>')
+
+
+
+
+
 
 
 @route('/title/<url:path>')
@@ -108,5 +118,5 @@ def parseFile():
 if __name__ == '__main__':
     publications = parseFile()
 
-    port = int(os.environ.get('PORT', 1337))
+    port = int(os.environ.get('PORT', 8080))
     run(host='0.0.0.0', port=port, debug=True)
